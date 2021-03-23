9c5ef3e894cfde69a32763b01faae8f3e4813806
Gabrielle Sloane Law <rockymcrockerson@gmail.com>
2021-03-22 22:32:11 -0700

    add timepicker

--- app/site/routes.py
+++ app/site/routes.py
@@ -8,7 +8,10 @@ CONFIG = Config()
 
 @site.route("/")
 def home():
-    return render_template("site/template.html", places_api_key=CONFIG.PLACES_API_KEY)
+    return render_template(
+        "site/template.html",
+        places_api_key=CONFIG.PLACES_API_KEY,
+    )
 
 
 @site.route("/about")
